328f63373dee6cc1124dee04f1b9734ee5f6f3ab
Kenyon Geetings <you@example.com>
2021-04-08 23:19:45 -0500

    Stuff for Test2

---
+++ In Class/FileIntroTest.py
@@ -0,0 +1,100 @@
+# file_var = open("TestWriteFile.txt", "r")
+
+# number = 5
+# for line in range(number):
+#    print(line, file=file_var)
+
+# print(file_var.read())
+# file_var.close()
+
+#x = set()
+#for value in range(5):
+    #x.add(value)
+
+#print(x)
+
+#Lists and stuff
+#listX = []
+#for value in range(11):
+    #listX.append(value)
+
+#listX.append(6)
+#print(listX)
+#print(listX[5:])
+#print(listX[:-1])
+#print(min(listX))
+#print(max(listX))
+#print(sum(listX))
+#print(len(listX))
+#listX.insert(5, 22)
+#print(listX)
+#listX.remove(6)
+#del(listX[5:7])
+#print(sorted(listX))
+#listX.sort()
+#print(listX)
+
+#listS = ["Hello","World","Ponys"]
+#listSFUCKER = "FUCKU".join(listS)
+#print(listSFUCKER)
+
+#x = 5
+#y = x
+
+#for i in range(5):
+    #y += 4
+
+
+#print(x)
+#print(y)
+
+#l = [1, 5, 9, 3]
+#h = l
+
+#h[0] = h[2]
+#h[2] = h[0]
+
+#print(h) # [9, 5, 9, 3]
+#print(l) # [9, 5, 9, 3]
+
+#h = h*2
+#print(h) # [9, 5, 1, 3, 9, 5, 1, 3]
+#print(l) # [9, 5, 1, 3]
+
+#tupleX = (5,8,1)
+#print(tupleX)
+
+#def student(firstname, lastname ='Mark', standard ='Fifth'):
+     #print(firstname, lastname, 'studies in', standard, 'Standard')
+ 
+## 1 keyword argument
+#student(firstname ='John')    
+ 
+## 2 keyword arguments                
+#student(firstname ='John', standard ='Seventh') 
+ 
+## 2 keyword arguments
+#student(lastname ='Gates', firstname ='John')
+
+#dictX = {}
+#dictY = {"blue":"5", "numb":"To the pain", "color": "Yellow"}
+
+#dictY["color"] = "I'M BLUE dabadeedaba die"
+#del dictY["color"]
+#print(dictX)
+#print(dictY["blue"])
+#print(dictY.items())
+#print(dictY.keys())
+#print(dictY.values())
+#print(len(dictY))
+
+#if "blue" in dictY:
+    #print("Hey, blue is here")
+
+#if "Green" in dictY:
+    #print("It's not, why did you think it would be, you dumbass")
+    
+#List is a collection which is ordered and changeable. Allows duplicate members.
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#Set is a collection which is unordered and unindexed. No duplicate members.
+#Dictionary is a collection which is ordered* and changeable. No duplicate members.
